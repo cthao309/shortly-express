3fb983bcc0f0eb4f1197ba5e5512f14c5560cb57
Charlie Thao <cthao309@gmail.com>
2020-04-19 22:11:59 -0700

    added cookieParser logic

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,6 +1,24 @@
 const parseCookies = (req, res, next) => {
 
-  res.cookie('test1 => ', 'hello world')
+  // res.cookie('test1 => ', 'hello world')
+
+  // retrieive the cookie from request or initialized to empty string
+  let cookieString = req.get('Cookie') || '';
+
+  // retreive all the fields in the cookie
+  let parsedCookies = cookieString.split('; ').reduce((cookies, cookie) => {
+    if(cookie.length) {
+      let indx = cookie.indexOf('=');
+      let token = cookie.slice(0, indx);
+      let key = cookie.slice(0, indx);
+
+      cookies[key] = token;
+    }
+
+    return cookies;
+  }, {});
+
+  req.cookies = parsedCookies;
 
   next();
 };
